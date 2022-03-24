0781ec47bd27129e1cb8c200f4c01a00f686002b
DrGoldenFlap <inmonmike@gmail.com>
2022-03-23 21:34:45 -0400

    Able to cature user title, body, author, and date.

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -3,11 +3,22 @@ import React, { useState } from 'react';
 const CreatePost= (props) => {
     const[title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const current = new Date();
+    const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const post = {title, body, author, date}
+        console.log(post)
+
+    }
 
     return (
         <div className='create'>
             <h2>Create New Post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Post Title:</label>
                 <input 
                     type='text'
@@ -15,15 +26,29 @@ const CreatePost= (props) => {
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                         />
-                <label>Post body</label>
+                <label>Post body:</label>
                 <textarea 
                     required
                     value= {body}
                     onChange={(e) => setBody(e.target.value)}
                     ></textarea>
+                    <label>Author:</label>
+                    <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Michael">Michael</option>
+                    <option value="Brent">Brent</option>
+                    <option value="Garret">Garret</option>
+                    <option value="Mitch">Mitch</option>
+                    <option value="Matthew">Matthew</option>
+                    <option value="Parker">Parker</option>
+                    <option value="Gary">Gary</option>
+                    <option value="Dustin">Dustin</option>
+                    </select>
                 <button>Add Post</button>
                 <p>{title}</p>
                 <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
